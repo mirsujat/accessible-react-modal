0d95e9ec228a544f41fc5c0c6726a0bceeac4cdd
Mir Sujat <mir.sujat@gmail.com>
2021-12-17 20:27:47 +0600

    mir

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -9,6 +9,7 @@ class Portal extends Component {
   constructor(props) {
     super(props);
     this.container = document.createElement("div");
+    
   }
 
   componentDidMount() {
@@ -26,14 +27,48 @@ class Portal extends Component {
 const Backdrop = props => <div className={props.className} {...props} />;
 
 class Modal extends Component {
+  constructor(props){
+    super(props);
+    this.focusRef = React.createRef();
+    this.setFocusRef = el =>{
+      if(this.props.open && el){
+        this.focusRef = el
+      }
+    }
+    this.state = { focus: false }
+  }
+
+  componentDidMount(){
+    this.setFocus();
+  }
+  setFocus = () =>{
+    if(this.props.open){
+      this.focusRef.focus();
+    }
+  }
+
+  onFocus = () =>{
+    if(this.props.open){
+      this.setState({focus: true});
+    }
+  }
+  onBlur = () =>{
+    if(this.props.open){
+       this.setState({focus: false});
+    }
+  }
+
   render() {
-    console.log("Props From Modal: ", this.props);
+   
     let content = null;
     let modalClass = "hidden";
     let backdropClass = "dialog-backdrop";
 
     if(!this.props.open){
        body.classList.remove("has-dialog");
+    }
+     if(this.props.open && this.props.label){
+        console.log("focusRef: ", this.focusRef);
     }
     if (this.props.open) {
       modalClass = "no-scroll";
@@ -46,7 +81,16 @@ class Modal extends Component {
                id="dialog1"
               aria-labelledby="dialog1_label"
               aria-modal="true" 
-              className={modalClass} open={this.props.open}>
+              className={modalClass} open={this.props.open}
+              >
+              <h4 className="dialog_label" 
+
+              ref={this.setFocusRef}
+              onFocus={this.onFocus}
+              onBlur={this.onBlur} 
+              tabIndex="0"
+              
+              >{this.props.label}</h4>
             {this.props.children}
           </div>
         </Fragment>
