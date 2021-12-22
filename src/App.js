39ddcf1fd5dba999ef0fc9706743f74852dcace3
Mir Sujat <mir.sujat@gmail.com>
2021-12-22 21:06:55 +0600

    mir

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import './App.css';
  class App extends Component{
    constructor(props){
     super(props);
-   
     this.state = { open: false}
    }
 
@@ -24,23 +23,23 @@ render(){
         Hello From Accessible React Modal
       </header>
       <section>
-        <button  onClick={this.openModal} >Open Modal</button>
+        <button id="focusAfterClose"  onClick={this.openModal} >Awesome Modal</button>
       </section>
       <section>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugiat minus nesciunt itaque molestiae nisi esse a amet praesentium in rerum non, cupiditate autem laboriosam, unde inventore quidem accusantium cumque.</p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugiat minus nesciunt itaque molestiae nisi esse a amet praesentium in rerum non, cupiditate autem laboriosam, unde inventore quidem accusantium cumque.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugiat minus nesciunt itaque molestiae nisi esse a amet praesentium in rerum non, cupiditate autem laboriosam, unde inventore quidem accusantium cumque.</p>  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugiat minus nesciunt itaque molestiae nisi esse a amet praesentium in rerum non, cupiditate autem laboriosam, unde inventore quidem accusantium cumque.</p>  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugiat minus nesciunt itaque molestiae nisi esse a amet praesentium in rerum non, cupiditate autem laboriosam, unde inventore quidem accusantium cumque.</p>  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugiat minus nesciunt itaque molestiae nisi esse a amet praesentium in rerum non, cupiditate autem laboriosam, unde inventore quidem accusantium cumque.</p>  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugiat minus nesciunt itaque molestiae nisi esse a amet praesentium in rerum non, cupiditate autem laboriosam, unde inventore quidem accusantium cumque.</p>  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugiat minus nesciunt itaque molestiae nisi esse a amet praesentium in rerum non, cupiditate autem laboriosam, unde inventore quidem accusantium cumque.</p>  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugiat minus nesciunt itaque molestiae nisi esse a amet praesentium in rerum non, cupiditate autem laboriosam, unde inventore quidem accusantium cumque.</p>  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugiat minus nesciunt itaque molestiae nisi esse a amet praesentium in rerum non, cupiditate autem laboriosam, unde inventore quidem accusantium cumque.</p>  
       </section>
-      <Modal isOpen={this.state.open}
+      <Modal 
+      isOpen={this.state.open}
+      onClick={this.closeModal}
       label="Awesome Modal"
+      focusAfterClose="focusAfterClose"
       >
        <section>
          <h4>This a awesome Modal</h4>
          <p>This is a paragraph from awesome modal</p>
-         <div className="content-footer">
-           <button  onClick={this.closeModal} >Close</button>
-           <button>Ok</button>
-         </div>
+         
        </section>
       </Modal>
     </div>
--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -29,36 +29,35 @@ class Modal extends Component {
   constructor(props){
     super(props);
     this.KEYCODE = { tab: 9, shift: 16 };
-    this.focusFirst = React.createRef();
+    //prev  element to be focus out side dialog
+    this.prevElRef = React.createRef();
+    //next element to be focus out side dialog
+    this.nextElRef = React.createRef();
+    // first focusable element inside dialog
     this.firstFocus = React.createRef();
+    // last focusable element inside dialog
     this.lastFocus = React.createRef();
-    this.dialog = null;
-     this.dialog1 = null || {};
-    this.setFocusRef = el =>{
-      if(this.props.isOpen && el){
-        this.focusFirst = el
-      }
-    }
-    this.state = { focus: false,  focusChanges : false}
+
+    //focusAfterClose
+    this.focusAfterClose = document.getElementById(this.props.focusAfterClose);
+    this.state = { focus: false}
   }
 
   componentDidMount(){
-    this.dialog = document.getElementById("modal-root");
     this.dialog1 = document.getElementById("dialog1");
-    console.log("dialog: ", this.dialog);
+    
   }
 
-  // componentDidUpdate(){
-  //   this.setFocus();
-  // }
-
- 
-
- 
+  componentDidUpdate(){
+    this.setFocus();
+  }
+  componentWillUnmount(){
+     this.focusAfterClose.focus();
+  }
 
   setFocus = () =>{
     if(this.props.isOpen){
-      this.focusFirst.current.focus();
+      this.firstFocus.current.focus();
     } 
   }
 
@@ -76,94 +75,19 @@ class Modal extends Component {
   onKeyUp = (e) =>{
     const rootNode = document.getElementById("dialog1");
     if(rootNode.hasChildNodes() ){
-      let childNodes = rootNode.childNodes;
-      let children = rootNode.children;
-      let firstChild = rootNode.firstChild;
-      let lastchild = rootNode.lastChild.lastChild;
-      let parentNode = rootNode.parentNode;
-      let previousSibling = rootNode.previousSibling;
-      let nextSibling = rootNode.nextSibling;
-      // this.focusFirstDescendant(rootNode);
-      this.focusLastDescendant(rootNode);
-
-    console.log("childNodes: ", childNodes);
-    // console.log("children: ", children);
-    // console.log("lastChild: ", lastchild);
-    // console.log("prevNode: ", parentNode);
-    // console.log("firstChild: ", firstChild);
-    // console.log("previousSibling: ", previousSibling);
-    // console.log("nextSibling: ", nextSibling);
-
+      if(rootNode.nextSibling.contains(e.target)){
+          this.setFocus();
+     }
+     if(rootNode.previousSibling.contains(e.target)){
+        this.lastFocus.current.focus();
+     }
     }
   }
 
-  
- focusFirstDescendant = function (element) {
-    for (var i = 0; i < element.childNodes.length; i++) {
-      var child = element.childNodes[i];
-      if (this.attemptFocus(child) ||
-          this.focusFirstDescendant(child)) {
-        return true;
-      }
-    }
-    return false;
-  }; // end focusFirstDescendant
-
-  /**
-   * @desc Find the last descendant node that is focusable.
-   * @param element
-   *          DOM node for which to find the last focusable descendant.
-   * @returns
-   *  true if a focusable element is found and focus is set.
-   */
-  focusLastDescendant = function (element) {
-    for (var i = element.childNodes.length - 1; i >= 0; i--) {
-      var child = element.childNodes[i];
-      if (this.attemptFocus(child) ||
-          this.focusLastDescendant(child)) {
-        return true;
-      }
-    }
-    return false;
-  }; // end focusLastDescendant
-
-  isFocusable = function (element) {
-    if (element.tabIndex > 0 || (element.tabIndex === 0 && element.getAttribute('tabIndex') !== null)) {
-      return true;
-    }
-  
-    if (element.disabled) {
-      return false;
-    }
-  
-    switch (element.nodeName) {
-      case 'A':
-        return !!element.href && element.rel !== 'ignore';
-      case 'INPUT':
-        return element.type !== 'hidden' && element.type !== 'file';
-      case 'BUTTON':
-      case 'SELECT':
-      case 'TEXTAREA':
-        return true;
-      default:
-        return false;
-    }
-  }; // End isFocusable
-  attemptFocus = function (element) {
-    if (!this.isFocusable(element)) {
-      return false;
-    }
-
-    this.setState({focusChanges : true });
-
-    try {
-      element.focus();
-    }
-    catch (e) {
-    }
-    this.setState({focusChanges : false });
-    return (document.activeElement === element);
-  }; // end attemptFocus
+  onClick = (e) =>{
+    this.props.onClick();
+    e.stopPropagation();
+  }
 
   render() {
    
@@ -174,9 +98,9 @@ class Modal extends Component {
 
     if(!this.props.isOpen){
        body.classList.remove("has-dialog");
+
     }
      if(this.props.isOpen && this.props.label){
-        console.log("focusFirst: ", this.focusFirst);
     }
     if (this.props.isOpen) {
       modalClass = "no-scroll";
@@ -184,8 +108,9 @@ class Modal extends Component {
       body.classList.add("has-dialog");
       content = (
         <div>
-           <Backdrop className={backdropClass} onClick={this.props.onClose}  >
-           <div id="first" className="focus_trap" tabIndex="0" ref={this.firstFocus}></div>
+           <Backdrop className={backdropClass} onClick={(e) => this.onClick(e)}  >
+           <div id="first" className="focus_trap" tabIndex="0" ref={this.prevElRef} 
+           onKeyUp={(e) =>  this.onKeyUp(e)}></div>
            <div role="dialog" 
                id="dialog1"
                aria-modal="true"
@@ -195,16 +120,23 @@ class Modal extends Component {
               onKeyUp={(e) =>  this.onKeyUp(e)}
               >
              
-              <h4 className="dialog_label" 
+              <h4 
+              id="dialog1_label"
+              className="dialog_label" 
               onClick={this.setFocus}
-              ref={this.focusFirst}
+              ref={this.firstFocus}
               tabIndex="0"
               >
                 {this.props.label}
               </h4>
             {this.props.children}
+            <div className="content-footer">
+                <button>Ok</button>
+                <button  onClick={ (e) => this.onClick(e)} ref={this.lastFocus}>Cancel</button>
             </div>
-          <div id="last" className="focus_trap" tabIndex="0" ref={this.lastFocus}></div>
+            </div>
+          <div id="last" className="focus_trap" tabIndex="0" ref={this.nextElRef}
+          onKeyUp={(e) =>  this.onKeyUp(e)}  ></div>
          </Backdrop>
         </div>
       );
